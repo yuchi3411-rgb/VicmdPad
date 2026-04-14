e09cb3eebc753b3e0722d1638652a233a54240c8
yuchi3411-rgb <yuchi3411@gmail.com>
2026-04-14 09:21:53 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Vicms bot ❤️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 2, 
       },
     ],
   },
